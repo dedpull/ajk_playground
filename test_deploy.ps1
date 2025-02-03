43b98e74a62de26956fca6b7dbf440e0b8c2b363
dedpull <ajkraft21@gmail.com>
2025-02-02 20:18:27 -0600

    Update test_deploy.ps1

--- test_deploy.ps1
+++ test_deploy.ps1
@@ -7,7 +7,7 @@ $OSLanguage = 'en-us'
 
 #Set Global OSDCloud Vars
 $Global:MyOSDCloud = [ordered]@{
-    Restart = [bool]$True
+    Restart = [bool]$False
     RecoveryPartition = [bool]$True
     OEMActivation = [bool]$True
     WindowsUpdate = [bool]$True
@@ -20,6 +20,29 @@ $Global:MyOSDCloud = [ordered]@{
     CheckSHA1 = [bool]$True
 }
 
+$OOBEDeployJson = @'
+{
+    "AddNetFX3":  {
+                      "IsPresent":  true
+                  },
+    "UpdateDrivers":  {
+                          "IsPresent":  true
+                      },
+    "UpdateWindows":  {
+                          "IsPresent":  true
+                      }
+}
+'@
+
+
 Write-Host "Start-OSDCloud -OSName $OSName -OSEdition $OSEdition -OSActivation $OSActivation -OSLanguage $OSLanguage -SkipAutopilot -SkipODT"
 Start-OSDCloud -OSName $OSName -OSEdition $OSEdition -OSActivation $OSActivation -OSLanguage $OSLanguage -SkipAutopilot -SkipODT
+
+If (!(Test-Path "C:\ProgramData\OSDeploy")) {
+    New-Item "C:\ProgramData\OSDeploy" -ItemType Directory -Force | Out-Null
+}
+$OOBEDeployJson | Out-File -FilePath "C:\ProgramData\OSDeploy\OSDeploy.OOBEDeploy.json" -Encoding ascii -Force
+
+Restart-Computer -Force
+
 
